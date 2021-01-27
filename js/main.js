85ef7f82b6cb5c0c7edcd2d09b67f7a2b120d34b
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-01-27 17:37:55 +0530

    front section initialised

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-//JS goes here
+const navbar = document.getElementById('navbar');
+const navbarItems = document.querySelectorAll('.navbar-item');
+
+window.onscroll = () => {
+    if (window.scrollY > 750) {
+        navbar.classList.add('scrolled');
+        navbarItems.forEach(item => {
+            item.classList.add('scrolled');
+        })
+    }
+    else {
+        navbar.classList.remove('scrolled');
+        navbarItems.forEach(item => {
+            item.classList.remove('scrolled');
+        })
+    }
+};
